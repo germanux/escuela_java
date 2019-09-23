bdbbeaec5d8375358a2ecec93666a1bc2161b47a
German Caballero <germanux@gmail.com>
2019-09-23 14:01:58 +0200

    Solucionando calculadora

--- 02_EjemplosHTML5/public_html/calculadora/calculadora.js
+++ 02_EjemplosHTML5/public_html/calculadora/calculadora.js
@@ -1,8 +1,26 @@
-let result = document.getElementById("resultado");
+class Calculadora {
+    constructor() {
+        this.result = document.getElementById("resultado");
+    }
+    numeroPulsado(eventObj) {
+        let valor = eventObj.currentTarget.innerHTML;
+        // alert("Pulsado " + valor);
+        if (valor ==="+/-") {
+            calculadora.result.value = "" + (- (parseFloat(calculadora.result.value)));
+        } else if (valor === "." ) {
+            if ( ! calculadora.result.value.includes(".")) 
+                calculadora.result.value += valor;
+        } else {
+            calculadora.result.value += valor;
+        }
+    }
+};
+let calculadora = null;
 
-function pulsado(valor) {
-    if (valor ==="-")
-        result.value = - (parseFloat(result.value));
-    else 
-        result.value += valor;
-}
+window.onload = function() {
+    let botones = document.getElementsByClassName("num"); // array de botones
+    calculadora = new Calculadora();
+    for (let boton of botones) {
+        boton.addEventListener("click", calculadora.numeroPulsado);
+    }
+};
