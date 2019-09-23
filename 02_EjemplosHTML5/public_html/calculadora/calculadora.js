dbaf742e983685da3ee7d3ed0874a0255ae0372b
German Caballero <germanux@gmail.com>
2019-09-23 16:09:02 +0200

    Haciendo operadores

--- 02_EjemplosHTML5/public_html/calculadora/calculadora.js
+++ 02_EjemplosHTML5/public_html/calculadora/calculadora.js
@@ -1,8 +1,16 @@
 class Calculadora {
     constructor() {
         this.result = document.getElementById("resultado");
+        this.anterior = document.getElementById("anterior");
+        this.mem = 0;
+        this.operador = "";
+        this.nuevoNum = false;
     }
     numeroPulsado(eventObj) {
+        if (this.nuevoNum) {
+            this.result.value = "0";
+            this.nuevoNum = false;
+        }
         let valor = eventObj.currentTarget.innerHTML;
         // alert("Pulsado " + valor);
         if (valor ==="+/-") {
@@ -14,15 +22,35 @@ class Calculadora {
             this.result.value += valor;
         }
     }
+    operadorPulsado(evObj) {
+        let operadorActual = evObj.currentTarget.innerHTML;        
+        
+        if (this.operador !== "") {
+            let valActual = parseFloat(this.result.value);
+            let resultado = eval(this.mem.toString() + this.operador + valActual);
+            this.result.value = resultado;
+        }
+        this.mem = parseFloat(this.result.value);
+        this.anterior.value = `${this.mem} ${operadorActual}`;
+        this.operador = operadorActual;
+        this.nuevoNum = true;
+    }
 };
 let calculadora = null;
 
 window.onload = function() {
-    let botones = document.getElementsByClassName("num"); // array de botones
     calculadora = new Calculadora();
+    
+    let botones = document.getElementsByClassName("num"); // array de botones
     for (let boton of botones) {
         boton.addEventListener("click", (evtObj) => { 
             calculadora.numeroPulsado(evtObj); 
         });
+    }
+    let botonesOp = document.getElementsByClassName("oper"); // array de botones
+    for (let btnOp of botonesOp) {
+        btnOp.onclick = (evObj) => {
+            calculadora.operadorPulsado(evObj);
+        };
     }
 };
